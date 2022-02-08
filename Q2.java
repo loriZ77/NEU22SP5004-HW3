c1c714ba57d3a5a52b1bc419c87d83c3ab7619a9
Yuyan <57802869+loriZ77@users.noreply.github.com>
2022-02-07 16:36:25 -0800

    Update Q2.java

--- Q2.java
+++ Q2.java
@@ -1,10 +1,11 @@
 /**
+ * Problem 2 [10pts]
  * Define an integer array with length as 6,
  * then double the element value which is at odd index.
  * Input: Integer array
  * Output: Integer array
  * Example: {1, 2, 3, 4, 5, 6} -> {1, 4, 3, 8, 5, 12}
  */
-public class Q2 {
+public class Problem2 {
 
 }
