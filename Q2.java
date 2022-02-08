f2f09e37731a5122c2261816c14edcaf16e7654b
Yuyan <57802869+loriZ77@users.noreply.github.com>
2022-02-07 16:14:10 -0800

    Add files via upload

---
+++ Q2.java
@@ -0,0 +1,12 @@
+package hw3;
+
+/**
+ * Define an integer array with length as 6,
+ * then double the element value which is at odd index.
+ * Input: Integer array
+ * Output: Integer array
+ * Example: {1, 2, 3, 4, 5, 6} -> {1, 4, 3, 8, 5, 12}
+ */
+public class Q2 {
+
+}
