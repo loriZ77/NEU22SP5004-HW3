560b526f89877eeecc067d6c80f70f49778342d9
Charles Lyu <75901841+TotallyNewGuy@users.noreply.github.com>
2022-02-08 09:07:26 -0800

    Update Problem3.java
    Change problem

--- Problem3.java
+++ Problem3.java
@@ -1,15 +1,23 @@
 /*
-* You are given two integers, x and y, which represent your current location on a Cartesian grid: (x, y). You are also given an array points where each points[i] = [ai, bi] represents that a point exists at (ai, bi). A point is valid if it shares the same x-coordinate or the same y-coordinate as your location.
-* Return the index (0-indexed) of the valid point with the smallest Manhattan distance from your current location. If there are multiple, return the valid point with the smallest index. If there are no valid points, return -1.
-* The Manhattan distance between two points (x1, y1) and (x2, y2) is absolute value of (x1 - x2) + absolute value of (y1 - y2).
-* 
-* Inputs: (1) int x and (2) int y representing current point coordinate. (3) 2D int array points representing all points you need to calculates distance. It is guaranteed that x, y  >= 1, the length of points >= 0.
-* Output: int res representing smallest index of point which is nearest point to your current location.
+* Write a program that finds the transpose of a matrix,
+* read from a text file(Problem3.txt)
+* The first line in the file will be the dimensions.
+* The rest of lines will be the matrix
 *
-* Example:
-* Input: x = 1, y = 1, points = [[2,2], [2,1], [4,5]]
-* Output: res = 1
-* Because the nearest point to [1,1] is [2,1], return index 1.
+* Input:
+* No parameter needed in your method, but there is a txt file 
+* you need to download and use for this problem.
+
+* txt file context example:
+*   3 4
+*   1 2 3 4
+*   2 3 5 2
+*   0 9 8 -2
+* (3 and 4 in the first line represent this is a 3 x 4 matrix)
+*
+* Output:
+* A string represent the transpose of a matrix
+* each line represent one row and one white space between each two elements.
 */
 
 public class Problem3 {
