3483e6d27cd4046e2ec411de16f91c72b6567a72
Charles Lyu <75901841+TotallyNewGuy@users.noreply.github.com>
2022-02-07 16:48:04 -0800

    Update Problem3.java

--- Problem3.java
+++ Problem3.java
@@ -2,8 +2,10 @@
 * You are given two integers, x and y, which represent your current location on a Cartesian grid: (x, y). You are also given an array points where each points[i] = [ai, bi] represents that a point exists at (ai, bi). A point is valid if it shares the same x-coordinate or the same y-coordinate as your location.
 * Return the index (0-indexed) of the valid point with the smallest Manhattan distance from your current location. If there are multiple, return the valid point with the smallest index. If there are no valid points, return -1.
 * The Manhattan distance between two points (x1, y1) and (x2, y2) is absolute value of (x1 - x2) + absolute value of (y1 - y2).
+* 
 * Inputs: (1) int x and (2) int y representing current point coordinate. (3) 2D int array points representing all points you need to calculates distance. It is guaranteed that x, y  >= 1, the length of points >= 0.
 * Output: int res representing smallest index of point which is nearest point to your current location.
+*
 * Example:
 * Input: x = 1, y = 1, points = [[2,2], [2,1], [4,5]]
 * Output: res = 1
